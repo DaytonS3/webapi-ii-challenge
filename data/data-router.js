19ed0472115cb7ff07117204bc5eb65c78044fad
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-23 16:01:18 -0400

    post complete with conditions

--- data/data-router.js
+++ data/data-router.js
@@ -1,17 +1,38 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./db.js');
+const db = require("./db.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-   db.find()
-   .then(post => {
-       res.json(post);
-   })
-   .catch(err => {
-       res.status(500).json({ error: "There was an error while saving the post to the database" })
-   }) 
-})
+router.get("/", (req, res) => {
+  db.find()
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
+    });
+});
 
-module.exports = router
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    db.insert({ title, contents })
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
+module.exports = router;
