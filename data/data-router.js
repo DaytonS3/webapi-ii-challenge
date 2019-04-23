47ea17aea3c36d097e08e3d1e8eab4489647c4c4
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-23 16:38:12 -0400

    Update Post complete

--- data/data-router.js
+++ data/data-router.js
@@ -37,6 +37,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// Add new Post
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
   if (!title || !contents) {
@@ -56,6 +57,7 @@ router.post("/", (req, res) => {
   }
 });
 
+//Delete Post by Id
 router.delete("/:id", (req, res) => {
   const UserId = req.params.id;
   db.remove(UserId)
@@ -71,6 +73,33 @@ router.delete("/:id", (req, res) => {
     .catch(err => {
       res.status(500).json({ error: "The post could not be removed" });
     });
+});
+
+//Update Post
+router.put("/:id", (req, res) => {
+  const UserId = req.params.id;
+  const updatePost = req.body;
+  if (!updatePost.title || !updatePost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    db.update(UserId, updatePost)
+      .then(post => {
+        if (!post) {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        } else {
+          res.status(200).json(post);
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "The post information could not be modified."
+        });
+      });
+  }
 });
 
 module.exports = router;
